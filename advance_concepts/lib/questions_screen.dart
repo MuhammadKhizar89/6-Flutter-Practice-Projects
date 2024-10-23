36b22fe80585fea1c926128d4b507bcec99cba1e
MuhammadKhizar89 <muhammadkhizar8919@gmail.com>
2024-10-23 23:34:40 +0500

    Map and spread operator

--- advance_concepts/lib/questions_screen.dart
+++ advance_concepts/lib/questions_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import "package:advance_concepts/answer_button.dart";
+import "package:advance_concepts/data/questions.dart";
 
 class QuestionScreen extends StatefulWidget {
   const QuestionScreen({super.key});
@@ -11,6 +13,25 @@ class QuestionScreen extends StatefulWidget {
 class _QuestionScreenState extends State<QuestionScreen> {
   @override
   Widget build(content) {
-    return const Center(child: Text("Question Screen"));
+    final currentQuestion = questions[0];
+
+    return SizedBox(
+      width: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            currentQuestion.question,
+            style: const TextStyle(color: Colors.white),
+          ),
+          const SizedBox(height: 30),
+          ...currentQuestion.options.map(
+            (option) {
+              return AnswerButton(onTap: () {}, answerText: option);
+            },
+          )
+        ],
+      ),
+    );
   }
 }
