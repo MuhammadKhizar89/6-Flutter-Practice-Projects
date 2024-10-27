4198a054b2eaa4b3f0d17b783f0eb48582ba2fc4
MuhammadKhizar89 <muhammadkhizar8919@gmail.com>
2024-10-28 00:56:56 +0500

    Expense Tracker

--- advance_concepts/lib/main.dart
+++
@@ -1,6 +0,0 @@
-import "package:flutter/material.dart";
-import "package:advance_concepts/quiz.dart";
-
-void main() {
-  runApp(const Quiz());
-}
--- advance_conceptss/lib/data/questions.dart
+++ advance_conceptss/lib/data/questions.dart
@@ -2,54 +2,75 @@ import "package:advance_conceptss/models/questions.dart";
 
 const questions = [
   Questions(
-    'What are the main building blocks of Flutter UIs?',
+    'What is the maximum number of overs allowed in a One Day International (ODI) match?',
     [
-      'Widgets',
-      'Components',
-      'Blocks',
-      'Functions',
-    ],
-  ),
-  Questions('How are Flutter UIs built?', [
-    'By combining widgets in code',
-    'By combining widgets in a visual editor',
-    'By defining widgets in config files',
-    'By using XCode for iOS and Android Studio for Android',
-  ]),
-  Questions(
-    'What\'s the purpose of a StatefulWidget?',
-    [
-      'Update UI as data changes',
-      'Update data as UI changes',
-      'Ignore data changes',
-      'Render UI that does not depend on data',
+      '50 overs',
+      '20 overs',
+      '100 overs',
+      '60 overs',
     ],
   ),
   Questions(
-    'Which widget should you try to use more often: StatelessWidget or StatefulWidget?',
+    'How many players are on the field for one team during a cricket match?',
     [
-      'StatelessWidget',
-      'StatefulWidget',
-      'Both are equally good',
-      'None of the above',
+      '11 players',
+      '10 players',
+      '12 players',
+      '9 players',
     ],
   ),
   Questions(
-    'What happens if you change data in a StatelessWidget?',
+    'What term is used for a bowler taking three wickets in three consecutive balls?',
     [
-      'The UI is not updated',
-      'The UI is updated',
-      'The closest StatefulWidget is updated',
-      'Any nested StatefulWidgets are updated',
+      'Hat-trick',
+      'Triple',
+      'Strike',
+      'Trifecta',
     ],
   ),
   Questions(
-    'How should you update data inside of StatefulWidgets?',
+    'Which country won the first-ever Cricket World Cup in 1975?',
     [
-      'By calling setState()',
-      'By calling updateData()',
-      'By calling updateUI()',
-      'By calling updateState()',
+      'West Indies',
+      'India',
+      'Australia',
+      'England',
+    ],
+  ),
+  Questions(
+    'How many runs are awarded if the ball crosses the boundary without touching the ground?',
+    [
+      '6 runs',
+      '4 runs',
+      '5 runs',
+      '3 runs',
+    ],
+  ),
+  Questions(
+    'Who has the highest individual score in a Test match?',
+    [
+      'Brian Lara',
+      'Sachin Tendulkar',
+      'Ricky Ponting',
+      'Jacques Kallis',
+    ],
+  ),
+  Questions(
+    'What is the term for a score of zero by a batsman?',
+    [
+      'Duck',
+      'Golden',
+      'Low score',
+      'Goose',
+    ],
+  ),
+  Questions(
+    'Which country has won the most ICC Cricket World Cups?',
+    [
+      'Australia',
+      'India',
+      'England',
+      'Pakistan',
     ],
   ),
 ];
