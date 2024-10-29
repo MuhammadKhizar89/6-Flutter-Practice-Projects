2bcc1729923ae28bf08293cc18b2a23b1ddb0b14
MuhammadKhizar89 <muhammadkhizar8919@gmail.com>
2024-10-30 02:03:52 +0500

    DropDown and Calender added

--- expense_tracker/lib/widgets/new_expense.dart
+++ expense_tracker/lib/widgets/new_expense.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import "package:expense_tracker/utils/date_formatter.dart";
+import "package:expense_tracker/models/expense.dart";
+
 class NewExpense extends StatefulWidget {
   const NewExpense({super.key});
   @override
@@ -8,11 +11,28 @@ class NewExpense extends StatefulWidget {
 }
 
 class _NewExpenseState extends State<NewExpense> {
-  var _textController = TextEditingController();
+  final _textController = TextEditingController();
+  final _amountController = TextEditingController();
+  DateTime? _selectedDate;
+  Category _selectedCategory = Category.leisure;
+
   @override
   void dispose() {
     _textController.dispose();
+    _amountController.dispose();
     super.dispose();
+  }
+
+  void _showDateModal() async {
+    var data = await showDatePicker(
+        context: context,
+        initialDate: DateTime.now(),
+        firstDate: DateTime(
+            DateTime.now().year - 1, DateTime.now().month, DateTime.now().day),
+        lastDate: DateTime.now());
+    setState(() {
+      _selectedDate = data;
+    });
   }
 
   @override
@@ -30,8 +50,66 @@ class _NewExpenseState extends State<NewExpense> {
           ),
           Row(
             children: [
+              Expanded(
+                child: TextField(
+                  controller: _amountController,
+                  keyboardType: TextInputType.number,
+                  decoration: const InputDecoration(
+                    prefixText: "\$ ",
+                    label: Text("Amount"),
+                  ),
+                ),
+              ),
+              const SizedBox(width: 16),
+              Expanded(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text(_selectedDate == null
+                        ? "No Date Chosen"
+                        : formattedDate(_selectedDate!)),
+                    IconButton(
+                      onPressed: _showDateModal,
+                      icon: const Icon(Icons.calendar_month),
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
+          Row(
+            children: [
+              const SizedBox(height: 70),
+              DropdownButton(
+                value: _selectedCategory,
+                items: Category.values
+                    .map(
+                      (cat) => DropdownMenuItem(
+                        value: cat,
+                        child: Text(cat.name.toUpperCase()),
+                      ),
+                    )
+                    .toList(),
+                onChanged: (value) {
+                  setState(
+                    () {
+                      _selectedCategory = value!;
+                    },
+                  );
+                },
+              ),
+              const Spacer(),
+              TextButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: const Text("Cancel"),
+              ),
               ElevatedButton(
-                  onPressed: () {},
+                  onPressed: () {
+                    print(_textController.text);
+                  },
                   child: Text("Add Expense"))
             ],
           )
